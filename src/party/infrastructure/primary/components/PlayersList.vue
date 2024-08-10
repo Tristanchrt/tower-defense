bfab3e661fc622109ee635cbb91a711bfea8536a
Tch <chretien.tristan1@gmail.com>
2024-08-10 16:30:10 +0200

    feat: rest

---
+++ src/party/infrastructure/primary/components/PlayersList.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="players">
+    <span class="player" :key="player.getName()" v-for="player in players">{{ player.getName() }}</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+import type { PropType } from 'vue'
+import type { Player } from '@/party/domain/Player'
+
+defineProps({
+  players: {
+    type: Array as PropType<Player[]>,
+    required: true
+  }
+})
+
+</script>
+
+<style scoped>
+.players {
+  margin-left: 3em;
+  display: flex;
+  flex-direction: column;
+}
+.player {
+  margin: 10px;
+  padding: 8px;
+}
+.player:hover {
+  background-color: grey;
+  margin: 10px;
+}
+</style>
--- src/party/infrastructure/primary/views/PartyPlayersToPlay.vue
+++ src/party/infrastructure/primary/views/PartyPlayersToPlay.vue
@@ -1,34 +1,36 @@
 <template>
-  <div class="party-players-to-play">`
+  <div class="party-players-to-play" v-if="party">`
     <div class="party-board">
       <h1>Party #{{ partyId }}</h1>
       <h3>Round #1</h3>
-      <BoardCard :board="party!.getBoard()" v-if="party" />
+      <BoardCard :board="party.getBoard()" />
     </div>
-    <div class="players" v-if="party">
-      <span class="player" :key="player.getName()" v-for="player in party!.getPlayers()">{{ player.getName() }}</span>
-    </div>
+    <PlayersList :players="party.getPlayers()"/>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { inject, onMounted, ref } from 'vue'
+import { inject, ref } from 'vue'
 import { useRoute } from 'vue-router'
 import type { Party } from '@/party/domain/Party'
 import { PartiesApplicationService } from '@/party/application/PartiesApplicationService'
 import BoardCard from '@/party/infrastructure/primary/components/BoardCard.vue'
+import PlayersList from '@/party/infrastructure/primary/components/PlayersList.vue'
 
 const partyId = ref<string | null>(null)
 const party = ref<Party>()
 
 const partyHandler = inject('partyApplicationService') as PartiesApplicationService
 
-onMounted(() => {
-  const route = useRoute()
+const route = useRoute()
+
+const fetchParty = () => {
   partyId.value = (route.params.id as string | undefined) ?? null
   party.value = partyHandler.getParties()[0]
-  console.log(party.value)
-})
+}
+
+fetchParty()
+
 </script>
 
 <style scoped>
@@ -37,17 +39,5 @@ onMounted(() => {
   justify-content: center;
   align-items: center;
 }
-.players {
-  margin-left: 3em;
-  display: flex;
-  flex-direction: column;
-}
-.player {
-  margin: 10px;
-  padding: 8px;
- }
-.player:hover {
-  background-color: grey;
-  margin: 10px;
-}
+
 </style>
